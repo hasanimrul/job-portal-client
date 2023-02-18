14e53768092ec9ac0f8956610125a1fba1df1fea
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2023-02-18 16:39:00 +0600

    contact us and about us done

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            about us
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img className='w-2/4' src="https://www.pentabell.com/wp-content/uploads/2022/08/620fc4613dae8186189936.jpg" alt='' />
+                    <div>
+                        <h1 className="text-5xl font-bold text-left">We Are The Job Portal Agency</h1>
+                        <p className="py-6 text-justify">On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/ContactUS/ContactUs.jsx
+++ src/Pages/ContactUS/ContactUs.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const ContactUs = () => {
     return (
         <div>
-            contact us
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src="https://media.istockphoto.com/id/1168945108/photo/close-up-image-of-male-hands-using-smartphone-with-icon-telephone-email-mobile-phone-and.jpg?s=612x612&w=0&k=20&c=aVojLzP1n3XNxuRdy7Pqdzo6OyRAVanOWDUWjbu3R8Q=" alt='' />
+                    <div className="">
+                        <h1 className="mb-5 text-5xl font-bold">Contact Information</h1>
+                        <p className="mb-5">Address: 198 West 21th Street, Suite 721 New York NY 10016</p>
+                        <p className="mb-5">Phone: + 1235 2355 98</p>
+                        <button className="btn btn-primary">Get Direction</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
